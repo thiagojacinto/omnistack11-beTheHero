3354c84eb2509023925987f622276a2f41bb4f23
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-24 17:48:02 -0300

    [feat] get() to list all NGOs + post() to insert a new NGO

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,42 @@
 const express = require('express');
-
+const crypto = require('crypto');
+const connection = require('./database/connection');
 const routes = express.Router();
 
 // all the routes here
 
-routes.get('/', (req, res) => {
-  // return res.send("Hello, we're starting Omnistack #11");  // plain text response
-
-  return res.json({   // JSON response
-    event: 'Omnistack Week #11.0',
-    author: 'Thiago Jacinto'
+/**
+ * CREATE A NEW NGO: POST() method
+ */
+routes.post('/ngos', async (request, response) => {
+  
+  const { name, email, phone, city, state } = request.body;
+  // to create and cryptograped id:
+  const id = crypto.randomBytes(4).toString('HEX');   
+  // stablish connection with db:
+  await connection('ngos').insert({
+    id,
+    name,
+    email,
+    phone,
+    city,
+    state
   });
+
+  return response.json({ id });
+
+});
+
+routes.get('/ngos/:limit', async(request, response) => {
+  // limit of the search:
+  const { limit } = request.params;
+  console.log(`Search limit: ${limit}`);
+  
+  // searchs everything (start) limiting results (limit(x))
+  const allNGOs = await connection('ngos').select('*').limit(limit);
+  // returns that json object:
+  return response.json(allNGOs);
+
 });
 
 

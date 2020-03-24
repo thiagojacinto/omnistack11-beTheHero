39dc723064ff68c9b6569ced6fe34badf08c963c
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-24 18:21:35 -0300

    [feat] new Incidents routes to create & index

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 const NGOController = require('./controllers/NgoController');
+const IncidentController = require('./controllers/IncidentController');
 
 // all the routes here
 
@@ -13,8 +14,16 @@ routes.post('/ngos', NGOController.create);
 /**
  * LIST NGOs INSERTED: GET() method
  */ 
-routes.get('/ngos/:limit', NGOController.listAll);
+routes.get('/ngos/:limit', NGOController.index);
 
+/**
+ * CREATES AN INCIDENT: POST()
+ */
+routes.post('/incidents', IncidentController.create);
+/**
+ * LIST INCIDENTS INSERTED: GET()
+ */
+routes.get('/incidents', IncidentController.index);
 
 // at the end, exports the routes:
 module.exports = routes;

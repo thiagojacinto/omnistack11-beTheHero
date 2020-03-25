d27a5ea4129b886ffcfdb43996c1c1147b6673f5
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-24 21:14:04 -0300

    [feat] incident + session controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,15 @@ const routes = express.Router();
 
 const NGOController = require('./controllers/NgoController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 // all the routes here
+
+/**
+ * SESSIONS
+ */
+routes.post('/session', SessionController.create);
 
 /**
  * CREATE A NEW NGO: POST() method
@@ -20,10 +27,21 @@ routes.get('/ngos/:limit', NGOController.index);
  * CREATES AN INCIDENT: POST()
  */
 routes.post('/incidents', IncidentController.create);
+
+
 /**
  * LIST INCIDENTS INSERTED: GET()
  */
 routes.get('/incidents', IncidentController.index);
+/**
+ * DELETE INCIDENT: delete()
+ */
+routes.delete('/incidents/:id', IncidentController.delete);
+
+/**
+ * GET INCIDENTS BY USER AUTHENTICATED
+ */
+routes.get('/profile', ProfileController.index);
 
 // at the end, exports the routes:
 module.exports = routes;

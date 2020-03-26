00c398b7c22a54d149bbcb6aaae188e8113af6fe
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-25 21:23:51 -0300

    [feat] routes to Profile & newIncident pages

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
   return (
@@ -11,6 +13,8 @@ export default function Routes() {
       <Switch>
         <Route path='/' exact component={Logon} />
         <Route path='/register' component={Register} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/profile/newincident' component={NewIncident} />
       </Switch>
     
     </BrowserRouter>

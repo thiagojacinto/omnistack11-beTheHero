50d2b943044ff876cf8bdabf50a5b1a3557588fe
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-25 21:19:49 -0300

    [new] newIncident page: component + css, initial

---
+++ web/src/pages/NewIncident/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg'
+
+import './newIncident.css';
+
+export default function NewIncident() {
+  return (
+    <div />
+  );
+}

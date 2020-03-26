1b4a4761058d77024e407f2d7c04e9eb622142aa
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-25 22:03:22 -0300

    [new] register new incident: component + css

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg'
 
@@ -8,6 +8,32 @@ import './newIncident.css';
 
 export default function NewIncident() {
   return (
-    <div />
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logo} alt="Be The Hero" />
+
+          <h1>Register new incident</h1>
+
+          <p>Register the incident with the information you want to share with volunteers.</p>
+
+          <Link className="default-link" to="/profile">
+            <FiArrowLeft size={20} color="#E02041" />
+            Back
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Title" />
+          <textarea placeholder="Description" />
+          <input placeholder="Value (Brazillian R$)" />
+
+          <button className="button" type="submit">
+            Register
+          </button>
+        </form>
+
+      </div>
+    </div>
   );
 }

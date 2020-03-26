6769c3f62a05fbee8e353bdf0e2a9dc4e68a3735
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-26 20:51:54 -0300

    [feat] logic to save new incidents, then returning to profile, reload the list

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg'
 
 import './newIncident.css';
 
 export default function NewIncident() {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ngoId = localStorage.getItem('ngoId');
+
+  const history = useHistory();
+
+  function handleNewIncident(event) {
+    event.preventDefault();
+
+    const data = {
+      title, description, value
+    }
+
+    try {
+
+      api.post('/incidents', data, {
+        headers: {
+          Authorization: ngoId,
+        }
+      });
+
+      history.push("/profile");
+
+    } catch (error) {
+      alert('Error: Incident not saved. Try again.')
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -23,12 +55,27 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Title" />
-          <textarea placeholder="Description" />
-          <input placeholder="Value (Brazillian R$)" />
+        <form onSubmit={handleNewIncident}>
+          <input 
+            placeholder="Title" 
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+          <textarea 
+            placeholder="Description" 
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+          />
+          <input 
+            placeholder="Value (Brazillian R$)" 
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
 
-          <button className="button" type="submit">
+          <button 
+            className="button" 
+            type="submit"
+          >
             Register
           </button>
         </form>
